9e1aeb4e95fed45d9da780216e14802daa7cdd9f
Rémy Kaloustian <remy.kaloustian@gmail.com>
2017-12-28 11:33:32 +0100

    Adding TasksContainer

---
+++ src/TasksContainer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/**
+ * A counter button: tap the button to increase the count.
+ */
+class TasksContainer extends React.Component {
+  constructor() {
+    super();
+   
+  }
+
+  render() {
+    return (
+      <h2>Tasks</h2>
+    );
+  }
+}
+export default TasksContainer;
