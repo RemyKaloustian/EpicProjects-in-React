2f5ce4b3f83ce64d6032fe726b7132758ac988cb
Rémy Kaloustian <remy.kaloustian@gmail.com>
2017-12-28 08:19:46 +0100

    Adding App.js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+/**
+ * A counter button: tap the button to increase the count.
+ */
+class App extends React.Component {
+  constructor() {
+    super();
+   
+  }
+
+  render() {
+    return (
+      <div id="container">
+       <h1>Epic Projects by Rémy KALOUSTIAN</h1>
+      </div>
+    );
+  }
+}
+export default App;
